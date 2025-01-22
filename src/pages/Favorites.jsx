19388f65fa1af320aa46d9dc02a3e426687a7a2c
Sachintha Dinuranga <117833055+Sachintha-Dinuranga@users.noreply.github.com>
2025-01-22 12:26:09 +0530

    add navbar component

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Favorites = () => {
+  return (
+    <div className="favorites-empty">
+      <h2>No favorite movies yet!!!</h2>
+      <p>Start adding movies to your favorite and it will apear here</p>
+    </div>
+  );
+};
+
+export default Favorites;
